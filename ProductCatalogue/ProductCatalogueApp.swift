c7436f684f4210737da9001897f69a71a3881643
Agus Adi <gusadi872@gmail.com>
2023-07-17 13:05:18 +0800

    Initial Commit

---
+++ ProductCatalogue/ProductCatalogueApp.swift
@@ -0,0 +1,20 @@
+//
+//  ProductCatalogueApp.swift
+//  ProductCatalogue
+//
+//  Created by Gus Adi on 17/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ProductCatalogueApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
