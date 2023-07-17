69812b7d5342fbd8a6272e33ac0d591ced5e2dff
Agus Adi <gusadi872@gmail.com>
2023-07-18 00:57:52 +0800

    New project structure

---
+++ ProductCatalogue/Feature/Catalogue/View/CatalogueView.swift
@@ -0,0 +1,20 @@
+//
+//  CatalogueView.swift
+//  ProductCatalogue
+//
+//  Created by Gus Adi on 18/07/23.
+//
+
+import SwiftUI
+
+struct CatalogueView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CatalogueView_Previews: PreviewProvider {
+    static var previews: some View {
+        CatalogueView()
+    }
+}
